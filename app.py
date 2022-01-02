432f56b4fe0dfde641fb4cbd78ce631564f69eed
yulin <yulin90625@gmail.com>
2022-01-02 22:46:42 +0800

    Recovery

--- app.py
+++ app.py
@@ -240,10 +240,6 @@ def show_fsm():
     return send_file("fsm.png", mimetype="image/png")
 
 
-# if __name__ == "__main__":
-    # local
-    # port = os.environ.get("PORT", 8000)
-    # app.run(host="0.0.0.0", port=port, debug=True)
-
-    # Heroku
-    # app.run()
+if __name__ == "__main__":
+    port = os.environ.get("PORT", 8000)
+    app.run(host="0.0.0.0", port=port, debug=True)
