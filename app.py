a23eb2da010919271212b34f54e396575feeced8
yulin <yulin90625@gmail.com>
2022-01-02 22:31:51 +0800

    Deploy

--- app.py
+++ app.py
@@ -241,5 +241,9 @@ def show_fsm():
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT", 8000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # local
+    # port = os.environ.get("PORT", 8000)
+    # app.run(host="0.0.0.0", port=port, debug=True)
+
+    # Heroku
+    app.run()
